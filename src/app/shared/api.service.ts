7e17e673e88d9dd602230e054c167f4272249835
prudhvivarma123 <103100669+prudhvivarma123@users.noreply.github.com>
2022-05-03 11:21:59 +0530

    crud

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http:HttpClient) { }
+
+  postEmployee(data:any){
+    return this._http.post<any>("http://localhost:3000/posts",data)
+    .pipe(map((res:any)=>{
+      return res;
+    }));
+  }
+
+  getEmployees(){
+    return this._http.get('http://localhost:3000/posts');
+  }
+
+}
